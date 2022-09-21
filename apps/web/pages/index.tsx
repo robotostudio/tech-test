e6b38f139a9f24652298af76b2d3ba34d7ab07ac
Jonathan Alford <jono@roboto.studio>
2022-09-21 16:58:31 +0100

    Removed UI and setup Chakra UI

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,49 @@
-import { Button } from "ui";
+import {
+  Box,
+  Container,
+  Heading,
+  Link as ChakraLink,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import Link from "next/link";
 
-export default function Web() {
+export default function Index() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <Box as="main" pt={32}>
+      <Container maxW="6xl">
+        <Heading>Roboto Tech Test</Heading>
+        <Text>
+          Build out the pages below, if you get stuck,{" "}
+          <ChakraLink
+            textDecoration="underline"
+            href="mailto:jono@roboto.studio"
+          >
+            email me
+          </ChakraLink>{" "}
+          or refer to the{" "}
+          <ChakraLink
+            textDecoration="underline"
+            href="https://github.com/sanity-io/next-sanity#example-minimal-blog-post-template"
+          >
+            documentation here
+          </ChakraLink>
+        </Text>
+        <Stack mt={4}>
+          <Link href="/blog" passHref>
+            <ChakraLink textDecoration="underline">Blog list page</ChakraLink>
+          </Link>
+          <Link href="/blog/hello-world" passHref>
+            <ChakraLink textDecoration="underline">Blog single page</ChakraLink>
+          </Link>
+          <Link href="/blog/press-release" passHref>
+            <ChakraLink textDecoration="underline">
+              Press release (list of tagged blog posts tagged with
+              &apos;press-release&apos;)
+            </ChakraLink>
+          </Link>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
