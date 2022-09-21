6f6c933abdca6c46ac627b43e333969692c22c21
Jonathan Alford <jono@roboto.studio>
2022-09-21 18:04:21 +0100

    Everything updated

--- apps/web/pages/index.jsx
+++ apps/web/pages/index.jsx
@@ -2,17 +2,17 @@ import {
   Box,
   Container,
   Heading,
-  Link as ChakraLink,
   Stack,
   Text,
+  Link as ChakraLink,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Index() {
   return (
-    <Box as="main" pt={32}>
+    <Box as="main" py={{ base: "16", md: "24" }}>
       <Container maxW="6xl">
-        <Heading>Roboto Tech Test</Heading>
+        <Heading mb={2}>Roboto Tech Test</Heading>
         <Text>
           Build out the pages below, if you get stuck,{" "}
           <ChakraLink
@@ -27,19 +27,26 @@ export default function Index() {
             href="https://github.com/sanity-io/next-sanity#example-minimal-blog-post-template"
           >
             documentation here
+          </ChakraLink>{" "}
+          the original test document can be{" "}
+          <ChakraLink
+            textDecoration="underline"
+            href="https://www.notion.so/robotostudio/Tech-Test-f9b7353178ff481abef7c316db5ded85"
+          >
+            found here
           </ChakraLink>
         </Text>
         <Stack mt={4}>
-          <Link href="/blog" passHref>
-            <ChakraLink textDecoration="underline">Blog list page</ChakraLink>
-          </Link>
           <Link href="/blog/hello-world" passHref>
             <ChakraLink textDecoration="underline">Blog single page</ChakraLink>
           </Link>
-          <Link href="/blog/press-release" passHref>
+          <Link href="/blog" passHref>
+            <ChakraLink textDecoration="underline">Blog list page</ChakraLink>
+          </Link>
+          <Link href="/blog/tags/sailing-ships" passHref>
             <ChakraLink textDecoration="underline">
-              Press release (list of tagged blog posts tagged with
-              &apos;press-release&apos;)
+              Sailing Ships (list of tagged blog posts tagged with &apos;Sailing
+              Ships&apos;)
             </ChakraLink>
           </Link>
         </Stack>
